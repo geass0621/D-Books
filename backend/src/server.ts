7c5891bdb1730c5604e90b0fd6e00490184561dc
DongCuong <geas0621@gmail.com>
2025-05-05 16:37:34 +0300

    Added: connection to mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import bookRoutes from './routes/books';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(bookRoutes);
 
-app.listen(3000);
+mongoose
+  .connect(process.env.MONGO_CONNECTION || '')
+  .then(() => {
+    console.log('connected');
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
