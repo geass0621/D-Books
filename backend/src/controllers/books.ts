3b3d195fea90ab29e932ebe92c942bd08b3ef186
DongCuong <geas0621@gmail.com>
2025-05-05 17:34:17 +0300

    Added: book model

--- backend/src/controllers/books.ts
+++ backend/src/controllers/books.ts
@@ -8,4 +8,8 @@ const books: any[] = [];
 
 export function getBooks(req: GetBooksRequest, res: GetBooksResponse, next: GetBooksNextFunction): void {
   res.status(200).json({ books: books });
+};
+
+export function postBook(req: GetBooksRequest, res: GetBooksResponse, next: GetBooksNextFunction): void {
+
 }
--- backend/src/models/book.ts
+++ backend/src/models/book.ts
@@ -1,4 +1,4 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 interface IBook {
   name: string;
@@ -6,5 +6,36 @@ interface IBook {
   description: string;
   price: number;
   discount: number;
-
+  imageUrl: string;
 }
+
+const bookSchema = new Schema<IBook>({
+  name: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  discount: {
+    type: Number,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+});
+
+const Book = model<IBook>('Book', bookSchema);
+
+export default Book
