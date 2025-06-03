1734d9f097563d4bb82dbae430419620491257f6
DongCuong <geas0621@gmail.com>
2025-06-03 18:32:17 +0300

    backend: added validation for order data

--- backend/src/routes/checkout.ts
+++ backend/src/routes/checkout.ts
@@ -1,11 +1,30 @@
 import { Router } from "express";
 import { postOrder, postPayment } from "../controllers/order";
 import { isAuth } from "../middleware/isAuth";
+import { body } from "express-validator";
 
 
 const router = Router();
 
-router.post('/checkout/order', isAuth, postOrder);
+router.post('/checkout/order', isAuth, [
+  body('shippingAddress')
+    .notEmpty()
+    .withMessage('Shipping address is required!'),
+  body('name')
+    .notEmpty()
+    .withMessage('Name is required!'),
+  body('email')
+    .trim()
+    .isEmail()
+    .withMessage('Please enter a valid email!')
+    .normalizeEmail(),
+  body('phone')
+    .notEmpty()
+    .withMessage('Phone number is required!'),
+  body('items')
+    .isArray({ min: 1 })
+    .withMessage('At least one item is required in the order!'),
+], postOrder);
 router.post('/checkout/payment', isAuth, postPayment);
 
 
