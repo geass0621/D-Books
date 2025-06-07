aedf44fbc10ccc61366039d17628bb78af028dcd
DongCuong <geas0621@gmail.com>
2025-06-07 16:07:27 +0300

    backend: added a build script

--- backend/src/middleware/allowCORS.ts
+++ backend/src/middleware/allowCORS.ts
@@ -13,4 +13,4 @@ export const allowCORS: RequestHandler = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-}
+};
